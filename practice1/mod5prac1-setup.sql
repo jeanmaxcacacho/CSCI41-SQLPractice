f3de9469d2449fc38a1ef6da368ac12aaccec5c9
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-10-16 19:47:02 +0800

    publisher and book table creation done

---
+++ practice1/mod5prac1-setup.sql
@@ -0,0 +1,31 @@
+-- yes I copied the HOA setup creation script
+drop database pubs;
+create database pubs;
+use pubs;
+
+drop table publisher;
+drop table book;
+
+create table publisher(
+    pub_id int auto_increment primary key,
+    name varchar(255) not null,
+    city varchar(255) default 'Quezon City',
+    state varchar(255) default 'NCR',
+    country varchar(255) default 'Philippines',
+    pr_info text
+);
+
+create table book(
+    title_id int auto_increment primary key,
+    title varchar(255) not null,
+    author_name varchar(255) not null,
+    author_address varchar(255),
+    author_contract varchar(255),
+    price float(2) not null,
+    advance float(2),
+    royalty float(2),
+    notes text,
+    pubdate date not null,
+    pub_id int,
+    foreign key (pub_id) references publisher(pub_id) on delete restrict
+);
